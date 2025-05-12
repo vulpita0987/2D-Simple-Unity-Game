a3be1f4d4d6787bc66a0787bf5f1c471d4d8cabb
vulpita0987 <114591107+vulpita0987@users.noreply.github.com>
2025-05-12 19:59:31 +0100

    Add files via upload

---
+++ CameraMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float cameraSpeed;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
+    }
+
+  
+}
